8e25d579bc5f498ee6344f7a3cfb6ae375d01e73
angienohava <angienohava03>
2024-05-30 12:00:07 -0500

    traimos al proyecto estilos de error 404 html

--- src/controllers/controller.home.js
+++ src/controllers/controller.home.js
@@ -1,6 +1,7 @@
 export const home = (req,res) => {
-    res.send("home")
+    // res.send("home")
+    res.render("views.home.ejs");
 }
 export const contact = (req,res) => {
-    res.send("contact")
+    res.render("views.contact.ejs")
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
 import express from "express";
 import { config } from "dotenv";
 import ruta from "./routes";
+import ejs from "ejs";
+import path from "path"
 config();
 
 const server = express();
 
 server.set("port", process.env.PORT || 3000);
+server.set('view engine', 'ejs')
+server.set('views', __dirname+'./views')
+
+server.set('views', path.join(__dirname,'views')); //el path.join es para que indiferentemente del sistema operativo sirva la ruta
+
+server.use(express.static(path.join(__dirname,'public')))
+
+
+// console.log(__dirname); para verificar la ruta que diga src
+
 server.use("/", ruta);
+
+//error 404
+server.use("/", (req, res) =>{
+    res.render("views.error404.ejs")
+})
+
+
 
 export default server;
